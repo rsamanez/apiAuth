7198c7752fb4af01c8759097ab610d5179d93ef6
Rommel Samanez <rsamanez@gmail.com>
2021-03-26 23:58:53 -0500

    laravel version update

--- config/app.php
+++ config/app.php
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Laravel\Passport\PassportServiceProvider::class, //added
         /*
          * Application Service Providers...
          */
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,4 +16,10 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::get('/tasks',function(){
+        return 'Hola Mundo';
+    });
 });
